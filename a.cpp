1806e0fcd7ce70024ae3dda7b7dc90d211ace2bc
Vincent Lam <lVincentLam@csu.fullerton.edu>
2019-02-06 00:50:02 -0800

    First time project on Linux + using git

---
+++ a.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void reverseArray(int const SIZE, int *array);
+
+int main()
+{
+  int const SIZE = 4;
+  int array[SIZE] = {0,0,0,0};
+
+  cout << "Input 4 numbers.\n";
+
+  for (int i = 0; i < SIZE; i++)
+  {
+    cin >> array[i];
+  }
+
+  cout << "The array you inputted was:\n";
+  for (int j = 0; j < SIZE; j++)
+  {
+    cout << array[j] << "\t";
+  }
+  reverseArray(SIZE, array);
+
+  cout << endl;
+  system("pause");
+  return 0;
+}
+
+void reverseArray(int const SIZE, int *array)
+{
+  cout << "\nYour array in reverse order is:\n";
+  for (int i = SIZE - 1; i < 0; i--)
+  {
+    cout << array[i] << "\t";
+  }
+}
